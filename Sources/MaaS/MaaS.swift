a1e90b7a8714269453dfef11dc79035e81984a2e
Viacheslav Platonov <slava.p12@yandex.ru>
2022-06-28 18:55:51 +0300

    no static

--- Sources/MaaS/MaaS.swift
+++ Sources/MaaS/MaaS.swift
@@ -19,7 +19,7 @@ public class MaaS {
     
     public static weak var networkDelegate: MaaSNetworkDelegate?
     
-    public static var token: String? = "1MLR6LtBXXNl2cx4swh1E3CMVv4RgcYJCEMdFFYXSQk"
+    public static var token: String? = "fmZh_UyaGDaQUI1hZrvDPxGo8_qIwq020qHXXHZHhYk"
     
     public var apiError: APIError?
     public var errorMessage: String?
